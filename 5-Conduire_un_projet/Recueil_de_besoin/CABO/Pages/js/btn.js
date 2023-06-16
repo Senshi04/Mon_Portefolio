4a152be20cb097bca9ed2ccebb28d19877d18a62
Senshi04 <125262209+Senshi04@users.noreply.github.com>
2023-06-17 00:50:01 +0200

    Add files via upload

---
+++ 5-Conduire_un_projet/Recueil_de_besoin/CABO/Pages/js/btn.js
@@ -0,0 +1,9 @@
+function hamMenuClick() {
+    document.getElementById("hamMenu").classList.toggle("active");
+    document.getElementById("ul").classList.toggle("link");
+}
+
+function galerieV() {
+	document.getElementById("li").classList.toggle("active");
+	document.getElementById("gv").classList.toggle("link");
+}
